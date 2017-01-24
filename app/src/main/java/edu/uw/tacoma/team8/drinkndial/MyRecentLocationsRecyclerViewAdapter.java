eb776f69f76510870693639fb65a96ac1e79f759
lovejithari <lovejitshari@gmail.com>
2017-01-24 13:16:53 -0800

    Uploading initial files to git

---
+++ app/src/main/java/edu/uw/tacoma/team8/drinkndial/MyRecentLocationsRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package edu.uw.tacoma.team8.drinkndial;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.uw.tacoma.team8.drinkndial.fragments.RecentLocationsFragment.OnListFragmentInteractionListener;
+import edu.uw.tacoma.team8.drinkndial.location.LocationContent;
+import edu.uw.tacoma.team8.drinkndial.location.LocationContent.LocationItem;
+
+import java.util.List;
+
+/**
+ * 
+ */
+public class MyRecentLocationsRecyclerViewAdapter extends RecyclerView.Adapter<MyRecentLocationsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<LocationItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyRecentLocationsRecyclerViewAdapter(List<LocationContent.LocationItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_recentlocations, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public LocationItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/edu/uw/tacoma/team8/drinkndial/fragments/TripsFragment.java
@@ -0,0 +1,15 @@
+package edu.uw.tacoma.team8.drinkndial.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.uw.tacoma.team8.drinkndial.R;
+
+public class TripsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips_fragment);
+    }
+}
