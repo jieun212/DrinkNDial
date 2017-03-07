9189c16f1ad611a5934c8b9312b3710b5552555f
Jieun Lee <eunitown@gmail.com>
2017-03-07 14:52:24 -0800

    Added DriversList, modified Signin

--- app/src/main/java/edu/uw/tacoma/team8/drinkndial/navigation/SettingsFragment.java
+++ app/src/main/java/edu/uw/tacoma/team8/drinkndial/navigation/SettingsFragment.java
@@ -1,36 +1,34 @@
 package edu.uw.tacoma.team8.drinkndial.navigation;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import edu.uw.tacoma.team8.drinkndial.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  *
  * @version 02/14/2017
  * @author  Jieun Lee (jieun212@uw.edu)
  */
 public class SettingsFragment extends Fragment {
 
-    private final String DEFAULT_MILES = "1";
+    /**
+     * An URL for getting prefer mile to find drivers
+     */
+    private final static String GET_PREFERENCE_URL
+            = "http://cssgate.insttech.washington.edu/~jieun212/Android/dndPreference.php?cmd=select";
 
     private TextView mNameTextView;
     private TextView mPhoneTextView;
     private TextView mEmailTextView;
-
-
-    private OnFragmentInteractionListener mListener;
+    private TextView mMileTextView;
+    private String mUserEamil;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -52,83 +50,56 @@ public class SettingsFragment extends Fragment {
         mPhoneTextView = (TextView) v.findViewById(R.id.setting_user_phone);
         mEmailTextView = (TextView) v.findViewById(R.id.setting_user_email);
 
-
         String homeAddress = getArguments().getString("homeaddress");
         String favoriteAddress = getArguments().getString("favoriteaddress");
 
+        mUserEamil = getArguments().getString("useremail");
         mNameTextView.setText(getArguments().getString("username"));
         mPhoneTextView.setText(getArguments().getString("userphone"));
-        mEmailTextView.setText(getArguments().getString("useremail"));
+        mEmailTextView.setText(mUserEamil);
 
 
         // add home button
         Button addHomeBtn =(Button) v.findViewById(R.id.add_home_button);
         if (homeAddress != null && homeAddress.length() > 0) {
-            addHomeBtn.setText("HOME: " + homeAddress);
+            addHomeBtn.setText(homeAddress);
         } else if (addHomeBtn.getText() == null || addHomeBtn.getText().toString().length() < 1) {
             addHomeBtn.setText("Add Home");
         }
-
-
             addHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((NavigationActivity) getActivity()).addHome();
+                ((NavigationActivity) getActivity()).goAddHome();
             }
         });
 
         // add location button
         Button addLocationBtn = (Button) v.findViewById(R.id.add_location_button);
         if (favoriteAddress != null && favoriteAddress.length() > 0) {
-            addLocationBtn.setText("FAVORITE: " + favoriteAddress);
+            addLocationBtn.setText(favoriteAddress);
         } else if (addLocationBtn.getText() == null || addLocationBtn.getText().toString().length() < 1) {
             addLocationBtn.setText("Add Favorite Location");
         }
-
             addLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((NavigationActivity) getActivity()).addLocation();
+                ((NavigationActivity) getActivity()).goAddLocation();
             }
         });
 
 
-        // driver preference
-        EditText prefermile = (EditText) v.findViewById(R.id.preference_miles);
-        prefermile.setText(DEFAULT_MILES);
-
+        // Mile preference
+        mMileTextView = (TextView) v.findViewById(R.id.preference_miles);
+        mMileTextView.setText(getArguments().getString("mile"));
+        Button editPreferButton = (Button) v.findViewById(R.id.mile_edit_button);
+        editPreferButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((NavigationActivity) getActivity()).goEditPreference();
+            }
+        });
         return v;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction();
-    }
 
 }
---
+++ app/src/main/java/edu/uw/tacoma/team8/drinkndial/util/SharedPreferenceEntry.java
@@ -0,0 +1,33 @@
+package edu.uw.tacoma.team8.drinkndial.util;
+
+/**
+ * Model class containing personal information that will be
+ * saved to SharedPreferences.
+ * Add to this class any other app related information
+ * that needs to be cached.
+ *
+ * Created by leejieun on 2/25/17.
+ */
+public class SharedPreferenceEntry {
+
+    // Name of the user.
+    private boolean mIsLoggedIn = false;
+
+    // Email address of the user.
+    private final String mEmail;
+
+    // Add others here..
+
+    public SharedPreferenceEntry(boolean loggedIn, String email) {
+        mIsLoggedIn = loggedIn;
+        mEmail = email;
+    }
+
+    public boolean isLoggedIn() {
+        return mIsLoggedIn;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+}
